38dfb9c2610b3e3226ce19ca40e916bbab472a40
staticWagomU <to6wa17@gmail.com>
2024-10-06 21:32:09 +0900

    feat: init

---
+++ index.js
@@ -0,0 +1,15 @@
+import { visit } from "unist-util-visit";
+import * as budoux from "budoux";
+
+export default function remarkBudoux() {
+  const parser = budoux.loadDefaultJapaneseParser();
+  return (tree) => {
+    visit(tree, 'text', (node) => {
+      if (node.value.trim()) {
+        node.value = parser.translateHTMLString(node.value);
+        node.type = 'html';
+      }
+    });
+  };
+}
+
