28adf3a7d59bb26e2001ebf62a637515a00b364c
staticWagomU <to6wa17@gmail.com>
2024-12-12 16:06:50 +0900

    style: biome format

--- index.js
+++ index.js
@@ -2,14 +2,13 @@ import { visit } from "unist-util-visit";
 import * as budoux from "budoux";
 
 export default function remarkBudoux() {
-  const parser = budoux.loadDefaultJapaneseParser();
-  return (tree) => {
-    visit(tree, 'text', (node) => {
-      if (node.value.trim()) {
-        node.value = parser.translateHTMLString(node.value);
-        node.type = 'html';
-      }
-    });
-  };
+	const parser = budoux.loadDefaultJapaneseParser();
+	return (tree) => {
+		visit(tree, "text", (node) => {
+			if (node.value.trim()) {
+				node.value = parser.translateHTMLString(node.value);
+				node.type = "html";
+			}
+		});
+	};
 }
-
